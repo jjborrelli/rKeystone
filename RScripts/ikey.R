4f4c72b99212150b626b8f8a1cb9545d906fa14e
Jon Borrelli <borrejj@gmail.com>
2017-01-11 17:06:52 -0500

    update data collection to include growth rates of removed and target spp

--- RScripts/ikey.R
+++ RScripts/ikey.R
@@ -80,18 +80,19 @@ spp_remove <- function(sc1, iP, t1, track.progress = FALSE){
   return(scl)
 }
 
-path_ints <- function(c1, sc1, scl){
+path_ints <- function(c1, sc1, scl, iP){
   spp <- which(sc1[nrow(sc1),] > 0)
   g1 <- graph.adjacency(abs(c1), weighted = T, diag = F)
   odegs <- degree(g1, mode = "out")
   idegs <- degree(g1, mode = "in")
   betw <- betweenness(g1, weights = E(g1)$weight)
-  
+  gr <- iP$alpha
   
   spaths <- matrix(nrow = length(spp), ncol = length(spp))
   ipaths.med <- matrix(0, nrow = length(spp), ncol = length(spp))
   ipaths.sum <- matrix(0, nrow = length(spp), ncol = length(spp))
   ipaths.mult <- matrix(0, nrow = length(spp), ncol = length(spp))
+  ipaths.sign <- matrix(0, nrow = length(spp), ncol = length(spp))
   for(i in 1:length(spp)){
     shp <- shortest_paths(g1, spp[i], spp, mode = "out", output = "epath")$epath
     spaths[i,] <- sapply(shp, length)
@@ -107,16 +108,17 @@ path_ints <- function(c1, sc1, scl){
     ipaths.med[i, spp %in% spp[-i]] <- sapply(ipa, median)
     ipaths.sum[i, spp %in% spp[-i]] <- sapply(ipa, sum)
     ipaths.mult[i, spp %in% spp[-i]] <- sapply(ipa, function(x) abs(prod(x))^(1/length(x))*sign(prod(x)))
-    
+    ipaths.sign[i, spp %in% spp[-i]] <- sapply(ipa, function(x) paste0(sign(x), collapse=","))
   
   }
   
   prs <- expand.grid(1:length(spp),1:length(spp))
-  res <- matrix(nrow = nrow(prs), ncol = 16)
+  res <- matrix(nrow = nrow(prs), ncol = 18)
+  ipathl <- list()
   for(i in 1:nrow(prs)){
     rspp <- which(spp %in% as.vector(spp)[prs[i,1]])
     if(nrow(scl[[rspp]]) < nrow(sc1)){
-      res[i,] <- c(rspp, rem = as.vector(spp)[prs[i,1]], trgt = as.vector(spp)[prs[i,2]],NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA)
+      res[i,] <- c(rspp, rem = as.vector(spp)[prs[i,1]], trgt = as.vector(spp)[prs[i,2]],NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA)
       next
     }
     res[i,] <- c(index = rspp, rem = as.vector(spp)[prs[i,1]], trgt = as.vector(spp)[prs[i,2]],
@@ -125,11 +127,14 @@ path_ints <- function(c1, sc1, scl){
                  pl = spaths[prs[i,1], prs[i,2]], imed = ipaths.med[prs[i,1], prs[i,2]], isum = ipaths.sum[prs[i,1], prs[i,2]], iprod = ipaths.mult[prs[i,1], prs[i,2]],
                  odeg.rem = odegs[as.vector(spp)[prs[i,1]]], odeg.trgt = odegs[as.vector(spp)[prs[i,2]]],
                  ideg.rem = idegs[as.vector(spp)[prs[i,1]]], ideg.trgt = idegs[as.vector(spp)[prs[i,2]]],
-                 bet.rem = betw[as.vector(spp)[prs[i,1]]], bet.trgt = betw[as.vector(spp)[prs[i,2]]])
+                 bet.rem = betw[as.vector(spp)[prs[i,1]]], bet.trgt = betw[as.vector(spp)[prs[i,2]]], 
+                 r.rem = gr[as.vector(spp)[prs[i,1]]], r.trgt = gr[as.vector(spp)[prs[i,2]]])
+    ipathl[[i]] <- ipaths.sign[prs[i,1], prs[i,2]]
   }
   colnames(res) <- c("index", "rem", "trgt", "bio.rem", "bio.trgt", "dbio.trgt", "pl", "imed", "isum", "iprod", "odeg.rem", "odeg.trgt", "ideg.rem", "ideg.trgt",
-                     "bet.rem", "bet.trgt")
+                     "bet.rem", "bet.trgt", "r.rem", "r.trgt")
   resdf <- as.data.frame(res)
+  resdf$isign <- unlist(ipathl)
   
   return(resdf)
 }
@@ -174,10 +179,13 @@ for(i in 1:10){
   e1 <- get_eqcomm(S = 50, C = .3, INTs = INTs, t1 = 1:2000, plot = F)
   sr1 <- spp_remove(sc1 = e1$comm.dyn, iP = e1$init.parms, t1 = 1:2000)
   
-  pi1 <- path_ints(c1 = e1$comm.mat, sc1 = e1$comm.dyn, scl = sr1)
+  pi1 <- path_ints(c1 = e1$comm.mat, sc1 = e1$comm.dyn, scl = sr1, iP = e1$init.parms)
   
   D.all[[i]] <- cdiv(sr1, e1$comm.dyn[2000,], meas = "shannon")
   i.all[[i]] <- itypes.sp(e1$comm.mat)[unique(pi1$rem),]
   
   print(i)
 }
+
+diff1 <- ((pi1$dbio.trgt - pi1$bio.trgt)/pi1$bio.trgt)*100
+aggregate(diff1, list(pi1$isign), median)
