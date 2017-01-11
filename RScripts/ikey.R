dc2a3ca1282cc2acacfa535cd92efbf6623ab4fa
Jon Borrelli <borrejj@gmail.com>
2017-01-11 16:15:49 -0500

    updated functions for data collection after sim and delta diversity

--- RScripts/ikey.R
+++ RScripts/ikey.R
@@ -53,8 +53,6 @@ sim_community <- function(times, state, parms, eq = lvmod, ex = ext1){
   return(out[,-1])
 }
 
-################################
-
 get_eqcomm <- function(S, C, INTs, t1, plot = FALSE){
   cond <- FALSE
   while(!cond){
@@ -85,6 +83,10 @@ spp_remove <- function(sc1, iP, t1, track.progress = FALSE){
 path_ints <- function(c1, sc1, scl){
   spp <- which(sc1[nrow(sc1),] > 0)
   g1 <- graph.adjacency(abs(c1), weighted = T, diag = F)
+  odegs <- degree(g1, mode = "out")
+  idegs <- degree(g1, mode = "in")
+  betw <- betweenness(g1, weights = E(g1)$weight)
+  
   
   spaths <- matrix(nrow = length(spp), ncol = length(spp))
   ipaths.med <- matrix(0, nrow = length(spp), ncol = length(spp))
@@ -105,27 +107,52 @@ path_ints <- function(c1, sc1, scl){
     ipaths.med[i, spp %in% spp[-i]] <- sapply(ipa, median)
     ipaths.sum[i, spp %in% spp[-i]] <- sapply(ipa, sum)
     ipaths.mult[i, spp %in% spp[-i]] <- sapply(ipa, function(x) abs(prod(x))^(1/length(x))*sign(prod(x)))
-
+    
+  
   }
   
   prs <- expand.grid(1:length(spp),1:length(spp))
-  res <- matrix(nrow = nrow(prs), ncol = 10)
+  res <- matrix(nrow = nrow(prs), ncol = 16)
   for(i in 1:nrow(prs)){
     rspp <- which(spp %in% as.vector(spp)[prs[i,1]])
     if(nrow(scl[[rspp]]) < nrow(sc1)){
-      res[i,] <- c(rspp, rem = as.vector(spp)[prs[i,1]], trgt = as.vector(spp)[prs[i,2]],NA,NA,NA,NA,NA,NA,NA)
+      res[i,] <- c(rspp, rem = as.vector(spp)[prs[i,1]], trgt = as.vector(spp)[prs[i,2]],NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA,NA)
       next
     }
     res[i,] <- c(index = rspp, rem = as.vector(spp)[prs[i,1]], trgt = as.vector(spp)[prs[i,2]],
                  bio.rem = as.vector(sc1[500,as.vector(spp)[prs[i,1]]]), bio.trgt = as.vector(sc1[500,as.vector(spp)[prs[i,2]]]),
                  dbio.trgt = as.vector(scl[[rspp]][500,as.vector(spp)[prs[i,2]]]),
-                 pl = spaths[prs[i,1], prs[i,2]], imed = ipaths.med[prs[i,1], prs[i,2]], isum = ipaths.sum[prs[i,1], prs[i,2]], iprod = ipaths.mult[prs[i,1], prs[i,2]])
+                 pl = spaths[prs[i,1], prs[i,2]], imed = ipaths.med[prs[i,1], prs[i,2]], isum = ipaths.sum[prs[i,1], prs[i,2]], iprod = ipaths.mult[prs[i,1], prs[i,2]],
+                 odeg.rem = odegs[as.vector(spp)[prs[i,1]]], odeg.trgt = odegs[as.vector(spp)[prs[i,2]]],
+                 ideg.rem = idegs[as.vector(spp)[prs[i,1]]], ideg.trgt = idegs[as.vector(spp)[prs[i,2]]],
+                 bet.rem = betw[as.vector(spp)[prs[i,1]]], bet.trgt = betw[as.vector(spp)[prs[i,2]]])
   }
-  colnames(res) <- c("index", "rem", "trgt", "bio.rem", "bio.trgt", "dbio.trgt", "pl", "imed", "isum", "iprod")
+  colnames(res) <- c("index", "rem", "trgt", "bio.rem", "bio.trgt", "dbio.trgt", "pl", "imed", "isum", "iprod", "odeg.rem", "odeg.trgt", "ideg.rem", "ideg.trgt",
+                     "bet.rem", "bet.trgt")
   resdf <- as.data.frame(res)
   
   return(resdf)
 }
+
+change_in_diversity <- function(eq.init, eq.removed, meas = "shannon"){
+  if(all(is.na(eq.removed))){return(NA)}
+  dr <- vegan::diversity(eq.removed, index = meas)
+  di <- vegan::diversity(eq.init, index = meas)
+  
+  deltaD <- ((dr - di)/di)*100
+  return(deltaD)
+}
+
+cdiv <- function(srcoms, eqi, meas = "shannon"){
+  eqsr <- lapply(srcoms, function(x){if(nrow(x) == 2000){x[2000,which(eqi != 0)]}else{NA}})
+  D <- sapply(eqsr, change_in_diversity, eq.init = eqi[which(eqi != 0)], meas = meas)
+  return(D)
+}
+
+
+##############################################################################
+##############################################################################
+##############################################################################
 
 
 SteinInt <- read.csv("~/Desktop/GitHub/microbial-dyn/Data/ecomod-ints.csv", row.names = 1)
@@ -139,5 +166,18 @@ sr1 <- spp_remove(sc1 = e1$comm.dyn, iP = e1$init.parms, t1 = 1:2000)
 pi1 <- path_ints(c1 = e1$comm.mat, sc1 = e1$comm.dyn, scl = sr1)
 head(pi1)
 
-diff1 <- (pi1$dbio.trgt - pi1$bio.trgt)/pi1$bio.trgt
-plot(diff1~pi1$iprod)
+summary(lm((cdiv(sr1, e1$comm.dyn[2000,], meas = "shannon")/100)~itypes.sp(e1$comm.mat)[unique(pi1$rem),]))
+
+D.all <- list()
+i.all <- list()
+for(i in 1:10){
+  e1 <- get_eqcomm(S = 50, C = .3, INTs = INTs, t1 = 1:2000, plot = F)
+  sr1 <- spp_remove(sc1 = e1$comm.dyn, iP = e1$init.parms, t1 = 1:2000)
+  
+  pi1 <- path_ints(c1 = e1$comm.mat, sc1 = e1$comm.dyn, scl = sr1)
+  
+  D.all[[i]] <- cdiv(sr1, e1$comm.dyn[2000,], meas = "shannon")
+  i.all[[i]] <- itypes.sp(e1$comm.mat)[unique(pi1$rem),]
+  
+  print(i)
+}
