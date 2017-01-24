6cd8e4c3701c703885ce67371c2ef9fa3bc7655d
Jon Borrelli <borrejj@gmail.com>
2017-01-24 15:52:43 -0500

    add cv to data collection during sim

--- RScripts/iloss.R
+++ RScripts/iloss.R
@@ -2,6 +2,9 @@ library(vegan)
 library(data.table)
 library(igraph)
 library(deSolve)
+library(dplyr)
+library(ggplot2)
+
 
 SteinInt <- read.csv("~/Desktop/GitHub/microbial-dyn/Data/ecomod-ints.csv", row.names = 1)
 #SteinInt <- read.csv("C:/Users/jjborrelli/Desktop/GitHub/microbial-dyn/Data/ecomod-ints.csv", row.names = 1)
@@ -11,44 +14,62 @@ INTs <- c(SteinInt[upper.tri(SteinInt)],SteinInt[lower.tri(SteinInt)])
 t0 <- Sys.time()
 Ns <- rep(c(10, 30, 50, 70, 90), each = 10)
 Cs <- rep(c(.05, .15, .25), each = 10)
-
+t.final <- 4000
 
 nc <- expand.grid(Ns, Cs)
 res <- list()
 inds <- list()
 for(i in 1:nrow(nc)){
-  eqc <- get_eqcomm(nc[i,1], nc[i,2], INTs, 1:2000)
+  eqc <- get_eqcomm(nc[i,1], nc[i,2], INTs, 1:t.final)
+  
+  cv10 <- apply(eqc$comm.dyn[1:10,], 2, function(x) sd(x)/mean(x))
+  cv100 <- apply(eqc$comm.dyn[1:100,], 2, function(x) sd(x)/mean(x))
+  cv1000 <- apply(eqc$comm.dyn[1:1000,], 2, function(x) sd(x)/mean(x))
+  
+  upptr.i <- mean(eqc$comm.mat[upper.tri(eqc$comm.mat)][eqc$comm.mat[lower.tri(eqc$comm.mat)] != 0])
+  lowtr.i <- mean(eqc$comm.mat[lower.tri(eqc$comm.mat)][eqc$comm.mat[lower.tri(eqc$comm.mat)] != 0])
+  
+  gr <- eqc$init.parms$alpha
+  iD <- vegan::diversity(eqc$comm.dyn[1,])
+  eq.exp <- -eqc$init.parms$alpha/diag(eqc$comm.mat)
   
   if(any(is.nan(eqc$comm.dyn))){
-    res[[i]] <- data.frame(co = i, N.ext = NA, t.bio = NA, exp.bio = NA, ShD = NA, iShD = NA, eig.i = NA, eig.f = NA,
-                           N = NA, C = NA, Lin = NA, Lfi = NA, 
-                           gr.in = NA, di.in = NA, (itypes(eqc$comm.mat)),
-                           gr.fi = NA, di.fi = NA, (itypes(eqc$comm.mat)))
+    res[[i]] <- data.frame(co = i, N.ext = NA, t.bio = NA, exp.bio = eq.exp, ShD = NA, iShD = iD, eig.i = NA, eig.f = NA,
+                           N = NA, C = NA, Lin = NA, Lfi = NA, gr.in = mean(gr), di.in = mean(diag(eqc$comm.mat)), (itypes(eqc$comm.mat)),
+                           gr.fi = NA, di.fi = NA, (itypes(eqc$comm.mat)), ut.in = upptr.i, ut.fi = NA, lt.in = lowtr.i, lt.fi = NA, 
+                           cv10 = median(cv10), cv100 = median(cv100), cv1k = median(cv1000))
     next
   }
   
-  eqc2 <- eqc$comm.mat[eqc$comm.dyn[2000,] != 0, eqc$comm.dyn[2000,] != 0]
-  gr <- eqc$init.parms$alpha
+  eqc2 <- eqc$comm.mat[eqc$comm.dyn[t.final,] > 0, eqc$comm.dyn[t.final,] > 0]
   
-  abund <- eqc$comm.dyn[2000,]
+  abund <- eqc$comm.dyn[t.final,]
+  abund[abund < 0] <- 0
+  
   ex <- sum(abund == 0)
   wex <- 1*(abund != 0)
-  eq.exp <- -eqc$init.parms$alpha/diag(eqc$comm.mat)
-  iD <- vegan::diversity(eqc$comm.dyn[1,])
+  
   D <- vegan::diversity(abund)
   
   Lin <- (sum(eqc$comm.mat != 0) - nrow(eqc$comm.mat))
   Lfi <- (sum(eqc2 != 0) - nrow(eqc2))
   
   jf1 <- rootSolve::jacobian.full(y = eqc$comm.dyn[1,], func = lvmod, parms = eqc$init.parms)
-  jf2 <- rootSolve::jacobian.full(y = eqc$comm.dyn[2000,eqc$comm.dyn[2000,] != 0], func = lvmod, parms = list(alpha = gr[eqc$comm.dyn[2000,] != 0], m = eqc2))
+  jf2 <- rootSolve::jacobian.full(y = eqc$comm.dyn[t.final,eqc$comm.dyn[t.final,] != 0], 
+                                  func = lvmod, parms = list(alpha = gr[eqc$comm.dyn[t.final,] != 0], m = eqc2))
+  
   eig1 <- max(Re(eigen(jf1)$values))
   eig2 <- max(Re(eigen(jf2)$values))
+  
+  upptr.f <- mean(eqc2[upper.tri(eqc2)][eqc2[upper.tri(eqc2)] != 0])
+  lowtr.f <- mean(eqc2[lower.tri(eqc2)][eqc2[lower.tri(eqc2)] != 0])
+  
   
   res[[i]] <- data.frame(co = i, N.ext = ex, t.bio = mean(abund), exp.bio = mean(eq.exp), ShD = D, iShD = iD, eig.i = eig1, eig.f = eig2,
                          N = nc[i,1], C = nc[i,2], Lin, Lfi, 
                          gr.in = mean(gr), di.in = mean(diag(eqc$comm.mat)), (itypes(eqc$comm.mat)),
-                         gr.fi = mean(gr[eqc$comm.dyn[2000,] != 0]), di.fi = mean(diag(eqc2)), (itypes(eqc2)))
+                         gr.fi = mean(gr[eqc$comm.dyn[t.final,] != 0]), di.fi = mean(diag(eqc2)), (itypes(eqc2)),
+                         ut.in = upptr.i, ut.fi = upptr.f, lt.in = lowtr.i, lt.fi = lowtr.f, cv10 = median(cv10), cv100 = median(cv100), cv1k = median(cv1000))
   
   i1 <- itypes.sp(eqc$comm.mat)
   im <- matrix(0, nrow = nc[i,1], ncol = 5)
@@ -69,7 +90,8 @@ for(i in 1:nrow(nc)){
   is.in2[as.logical(wex)] <- rowMeans(eqc2)
   
   inds[[i]] <- data.frame(co = i, n = nc[i,1], C = nc[i,2], ext = wex, bio = abund, ebio = eq.exp, self = diag(eqc$comm.mat), gro = gr, 
-                          bet.i = b1, bet.f = b2, i1, im, is.out1 = is.out, is.in1 = is.in, is.out2 = is.out2, is.in2 = is.in2)
+                          bet.i = b1, bet.f = b2, i1, im, is.out1 = is.out, is.in1 = is.in, is.out2 = is.out2, is.in2 = is.in2, 
+                          cv10 = cv10, cv100 = cv100, cv1k = cv1000)
   
   cat(rep(c(i, "\n"), 10))
 }
@@ -79,6 +101,8 @@ sppres <- rbindlist(inds)
 
 t1 <- Sys.time()
 t1-t0
+
+save.image("~/Desktop/simstab.Rdata")
 
 plot(ext~bet.i, data = sppres)
 cartA <- rpart::rpart(ext~n+C+self+gro+bet.i+comp+mut+pred+amens+comm+is.out1+is.in1, data = sppres, method = "class")
@@ -91,7 +115,7 @@ text(cartB, use.n=TRUE, all=TRUE, cex=.8)
 
 
 dfA <- data.frame(P = webres$N.ext/webres$N, C = webres$C, iShD = webres$iShD,t(apply(select(webres, comp:comm),1,function(x) x/sum(x))))
-
+dfB <- data.frame(t(apply(select(webres, comp:comm),1,function(x) x/sum(x))), t(apply(select(webres, comp.1:comm.1),1,function(x) x/sum(x))))
 head(melt(t(apply(select(webres, comp:comm),1,function(x) x/sum(x)))))
 test <- (data.frame(melt(t(apply(select(webres, comp.1:comm.1),1,function(x) x/sum(x)))), melt(t(apply(select(webres, comp:comm),1,function(x) x/sum(x))))))
 test2 <- (data.frame(melt(t(apply(select(sppres[as.logical(sppres$ext)], comp.1:comm.1),1,function(x) x/sum(x)))), melt(t(apply(select(sppres[as.logical(sppres$ext)], comp:comm),1,function(x) x/sum(x))))))
