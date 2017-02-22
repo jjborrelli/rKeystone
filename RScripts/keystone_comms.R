e2ac6a902c2868124bb815c03a0f281f3ff955f2
Jon Borrelli <borrejj@gmail.com>
2017-02-22 09:09:58 -0500

    pca for each

--- RScripts/keystone_comms.R
+++ RScripts/keystone_comms.R
@@ -19,6 +19,7 @@ library(data.table)
 lvmodK <- function(times, state, parms){
   with(as.list(c(state, parms)), {
     dB <- state * parms$alpha * (1 - state/(parms$K/sum(state > 0))) + state * parms$m %*% state 
+    #dB <- state * parms$alpha * (1 - state/(parms$K)) + state * parms$m %*% state 
     
     list(dB)
   })
@@ -155,29 +156,30 @@ coefvar <- function(dyna){
 
 remove.sp <- function(sp, parms, states){
   rbio <- states[sp]
+  rbio.rel <- states[sp]/sum(states)
   states[sp] <- 0
-  test <- ode(states, 1:2000, parms = parms, func = lvmodK, events = list(func = ext1, time =  1:2000))
+  test <- ode(states, 1:4000, parms = parms, func = lvmodK, events = list(func = ext1, time =  1:4000))
   print(matplot(test[,-1], typ = "l"))
   
-  if(nrow(test) == 2000){
+  if(nrow(test) == 4000){
     bi <- betweenness(graph.adjacency(abs(sign(parms$m))))
     br <- rep(NA, length(bi))
     br[-sp] <- betweenness(graph.adjacency(abs(sign(parms$m[-sp,-sp]))))
     bf <- rep(NA, length(bi))
-    bf[test[2000,-1]!=0] <- betweenness(graph.adjacency(abs(sign(parms$m[test[2000,-1]!=0,test[2000,-1]!=0]))))
+    bf[test[4000,-1]!=0] <- betweenness(graph.adjacency(abs(sign(parms$m[test[4000,-1]!=0,test[4000,-1]!=0]))))
     
     di <- degree(graph.adjacency(abs(sign(parms$m))))
     dr <- rep(NA, length(di))
     dr[-sp] <- degree(graph.adjacency(abs(sign(parms$m[-sp,-sp]))))
     df <- rep(NA, length(di))
-    df[test[2000,-1]!=0] <- degree(graph.adjacency(abs(sign(parms$m[test[2000,-1]!=0,test[2000,-1]!=0]))))
+    df[test[4000,-1]!=0] <- degree(graph.adjacency(abs(sign(parms$m[test[4000,-1]!=0,test[4000,-1]!=0]))))
     
     
-    sppdf <- data.frame(spR = sp, spT = 1:nrow(parms$m), coefvar(test[,-1]), sprBIO = rbio, iBio = test[1,-1],
-                        t10Bio = test[10,-1], t100Bio = test[100,-1], fBio = test[2000,-1],
-                        iBet = bi, rBet = br, fBet = bf, iDeg = di, rDeg = dr, fDeg = df,
+    sppdf <- data.frame(spR = sp, spT = 1:nrow(parms$m), coefvar(test[,-1]), sprBIO = rbio, sprREL = rbio.rel, iBio = test[1,-1],
+                        t10Bio = test[10,-1], t100Bio = test[100,-1], fBio = test[4000,-1], iBr = test[1,-1]/sum(test[1,-1]),
+                        fBr = test[4000,-1]/sum(test[4000,-1]), iBet = bi, rBet = br, fBet = bf, iDeg = di, rDeg = dr, fDeg = df,
                         gr = parms$alpha, self = diag(parms$m), 
-                        exts = (test[2000,-1] != 0))
+                        exts = (test[4000,-1] != 0))
   }else{
     sppdf <- NA
   }
@@ -185,21 +187,6 @@ remove.sp <- function(sp, parms, states){
   return(sppdf)
 }
 
-df1 <- list()
-for(x in unique(persist$comm)){
-  gr <- ge$eqgr[[x]]
-  m <- ge$eqm[[x]]
-  st1 <- ge$eqst[[x]]
-  mre <- c(NA)
-  mim <- c(NA)
-  for(i in persist$spr[persist$comm == x]){
-    jf <- jacobian.full(st1[-i], func = lvmodK, parms = list(alpha = gr[-i], m = m[-i,-i], K = 20))
-    mre[i] <- max(Re(eigen(jf)$values))
-    mim[i] <- Im(eigen(jf)$values)[which.max(Re(eigen(jf)$values))]
-  }
-  df1[[x]] <- data.frame(comm = x, sp = persist$spr[persist$comm == x], mre = mre[!is.na(mre)], mim = mim[!is.na(mre)])
-  print(x)
-}
 
 impacts <- function(ra, ge){
   persist <- aggregate(ra$exts, list(spr = ra$spR, comm = ra$comm), function(x) sum(x)/(length(x)-1))
@@ -223,6 +210,7 @@ impacts <- function(ra, ge){
   degs <- apply(degs, 1, function(x) x$x[x$spr[1]])
   
   rbio <- aggregate(ra$sprBIO, list(spr = ra$spR, comm = ra$comm), function(x) median(x))
+  relbio <- aggregate(ra$sprREL, list(spr = ra$spR, comm = ra$comm), function(x) median(x))
   
   c10 <- aggregate(ra$cv10, list(spr = ra$spR, comm = ra$comm), function(x) median(x, na.rm = T))$x
   c100 <- aggregate(ra$cv100, list(spr = ra$spR, comm = ra$comm), function(x) median(x, na.rm = T))$x
@@ -232,9 +220,15 @@ impacts <- function(ra, ge){
   d1 <- aggregate(ra$iBio, list(spr = ra$spR, comm = ra$comm), function(x) vegan::diversity(x))$x
   d2 <- aggregate(ra$fBio, list(spr = ra$spR, comm = ra$comm), function(x) vegan::diversity(x))$x
   
+  d1a <- aggregate(ra$iBio, list(spr = ra$spR, comm = ra$comm), function(x) vegan::diversity(x, "simpson"))$x
+  d2a <- aggregate(ra$fBio, list(spr = ra$spR, comm = ra$comm), function(x) vegan::diversity(x, "simpson"))$x
+  
+  d1b <- aggregate(ra$iBio, list(spr = ra$spR, comm = ra$comm), function(x) vegan::diversity(x, "inv"))$x
+  d2b <- aggregate(ra$fBio, list(spr = ra$spR, comm = ra$comm), function(x) vegan::diversity(x, "inv"))$x
+  
   eigs <- do.call(rbind, eigs)
   
-  dfall <- data.frame(persist, dbioN = dbio$x[,1], dbioP = dbio$x[,2], c10, c100, c1k, div = d2-d1, degs = degs, eigre = eigs$mre, eigim = eigs$mim, rbio = rbio$x)
+  dfall <- data.frame(persist, dbioN = dbio$x[,1], dbioP = dbio$x[,2], c10, c100, c1k, div = (d2-d1)/d1, simp = (d2a-d1a)/d1a, invs = (d2b-d1b)/d1b, degs = degs, eigre = eigs$mre, eigim = eigs$mim, rbio = rbio$x, rrel = relbio$x)
   
   return(dfall)
 }
@@ -242,12 +236,12 @@ impacts <- function(ra, ge){
 #################################################################################################
 #################################################################################################
 #################################################################################################
-
-SteinInt <- read.csv("~/Desktop/GitHub/microbial-dyn/Data/ecomod-ints.csv", row.names = 1)
-INTs <- c(SteinInt[upper.tri(SteinInt)],SteinInt[lower.tri(SteinInt)])
+# SteinInt <- read.csv("C:/Users/jjborrelli/Desktop/GitHub/microbial-dyn/Data/ecomod-ints.csv", row.names = 1)
+#SteinInt <- read.csv("~/Desktop/GitHub/microbial-dyn/Data/ecomod-ints.csv", row.names = 1)
+#INTs <- c(SteinInt[upper.tri(SteinInt)],SteinInt[lower.tri(SteinInt)])
 
 S = 100
-multityp <- lapply(1:50, function(x){
+multityp <- lapply(1:5, function(x){
   p1 <- runif(1,0,1)
   p2 <- runif(1, p1, 1)
   c1 <- runif(1, .1, .3)
@@ -264,8 +258,8 @@ ge.mult2 <- get_eq(multityp, times = 4000, INTs = INTs, Rmax = 3)
 a <- sapply(ge.mult$eqst, length)
 b <- sapply(ge.mult1$eqst, length)
 d <- sapply(ge.mult2$eqst, length)
-anova(lm(c(a,b,d)~c(rep("a",5),rep("b",5),rep("d",5))))
-boxplot(c(a,b,d)~c(rep("a",5),rep("b",5),rep("d",5)))
+anova(lm(c(a,b,d)~c(rep("a",length(a)),rep("b",length(b)),rep("d",length(d)))))
+boxplot(c(a,b,d)~c(rep("a",length(a)),rep("b",length(b)),rep("d",length(d))))
 
 sapply(ge.mult$eqst, function(x) {(20/length(x))})
 sapply(ge.mult1$eqst, function(x) {(20/length(x))})
@@ -275,6 +269,10 @@ sapply(ge.mult2$eqst, function(x) {(20/length(x))})
 ra.mult <- remove_all(ge.mult)
 
 im.mult <- impacts(ra.mult, ge.mult)
+
+pcM <- princomp(im.mult[-c(1,2,12,15,16,17)])
+loadings(pcM)
+
 
 e2 <- list()
 eA <-c()
@@ -307,7 +305,7 @@ for(x in 1:length(ge.comp$eqm)){
 #################################################################################################
 
 S = 100
-comptyp <- lapply(1:50, function(x){
+comptyp <- lapply(1:5, function(x){
   c1 <- runif(1, .1, .3)
   mats <- get.adjacency(erdos.renyi.game(S, c1, "gnp", directed = F), sparse = F)
   tat <- mats*-1
@@ -318,7 +316,10 @@ ge.comp <- get_eq(comptyp, times = 4000, INTs = INTs)
 ra.comp <- remove_all(ge.comp)
 im.comp <- impacts(ra.comp, ge.comp)
 
-predtyp <- lapply(1:50, function(x){
+pcC <- princomp(im.comp[-c(1,2,12,15,16,17)][complete.cases(im.comp),])
+loadings(pcC)
+
+predtyp <- lapply(1:5, function(x){
   c1 <- runif(1, .1, .3)
   mats <- get.adjacency(erdos.renyi.game(S, c1, "gnp", directed = T), sparse = F)
   for(i in 1:nrow(mats)){for(j in 1:ncol(mats)){if(mats[i,j] == 1){mats[j,i] <- -1}else{next}}}
@@ -329,9 +330,11 @@ ge.pred <- get_eq(predtyp, times = 4000, INTs = INTs)
 ra.pred <- remove_all(ge.pred)
 im.pred <- impacts(ra.pred, ge.pred)
 
+pcP <- princomp(im.pred[-c(1,2,12,15,16,17)])
+loadings(pcP)
 
-mututyp <- lapply(1:50, function(x){
-  c1 <- runif(1, .1, .3)
+mututyp <- lapply(1:5, function(x){
+  c1 <- .2#runif(1, .1, .3)
   mats <- get.adjacency(erdos.renyi.game(S, c1, "gnp", directed = F), sparse = F)
   return((mats))
 })
@@ -339,3 +342,21 @@ mututyp <- lapply(1:50, function(x){
 ge.mutu <- get_eq(mututyp, times = 4000, INTs = INTs*.2)
 ra.mutu <- remove_all(ge.mutu)
 im.mutu <- impacts(ra.mutu, ge.mutu)
+
+
+#################################################################################################
+#################################################################################################
+#################################################################################################
+
+testA <- rbind(cbind(im.mult, typ = "mult"), cbind(im.comp, typ = "comp"), cbind(im.pred, typ = "pred"))
+pcA <- princomp(testA[-c(1,2,12,15,16,17)])
+summary(pcA)
+loadings(pcA)
+plot(pcA$scores[,1]~testA$rbio)
+plot(pcA$scores[,1]~testA$rrel)
+cor.test(pcA$scores[,1],testA$rrel)
+
+ggplot(data.frame(pc1 = pcA$scores[,1], rela = testA$rrel, typ = testA$typ), aes(x = rela, y = pc1)) + geom_point(size = 3, aes(col = typ)) + facet_grid(~typ)
+
+ggplot(testA, aes(x = dbioN, y = dbioP)) + geom_point() + facet_grid(~typ)
+ggplot(testA, aes(x = dbioN, y = dbioP)) + geom_point(aes(col = typ)) 
